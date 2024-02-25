f3c5a237e275f34af21d4eb1465a6208a46db3ae
NikiRich <bogatyveron3@gmail.com>
2024-02-26 00:09:09 +0200

    v. 0.0.8
    -Added styles to the Products pages
    -Added script for redirection

---
+++ wwwroot/js/Products.js
@@ -0,0 +1,47 @@
+﻿let Whiskey = document.getElementById('Whiskey');
+let Beer = document.getElementById('Beer');
+let Wine = document.getElementById('Wine');
+let Vodka = document.getElementById('Vodka');
+let Tequila = document.getElementById('Tequila');
+let Rum = document.getElementById('Rum');
+let Gin = document.getElementById('Gin');
+let Champagne = document.getElementById('Champagne');
+let Liquor = document.getElementById('Liquor');
+
+Whiskey.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Whiskey";
+})
+
+Beer.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Beer";
+})
+
+Wine.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Wine";
+})
+
+Vodka.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Vodka";
+})
+
+Tequila.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Tequila";
+})
+
+Rum.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Rum";
+})
+
+Gin.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Gin";
+})
+
+Champagne.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Champagne";
+})
+
+Liquor.addEventListener("click", function () {
+    window.location.href = "/AlcInventory/Liquor";
+})
+
+
