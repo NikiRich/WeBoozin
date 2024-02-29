e3053ce8b6d1f9ec28f182e147398b7cfa8b429f
NikiRich <bogatyveron3@gmail.com>
2024-02-29 20:46:36 +0200

    v. 0.1.1
    -Added info in DB Context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WeBoozin.Models;
 namespace WeBoozin.Data
 
 {
@@ -7,6 +8,12 @@ namespace WeBoozin.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Tracker> Tracker { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
