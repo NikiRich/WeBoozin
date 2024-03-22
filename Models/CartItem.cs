e56014c7f830e72d3d3a12bf4821f5c9126e724a
NikiRich <bogatyveron3@gmail.com>
2024-03-22 11:28:53 +0200

    v. 0.2.2
    - Added a new CartItem model

---
+++ Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WeBoozin.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+
+        [ForeignKey("Cart")]
+        public int CartId { get; set; }
+        public virtual Cart Cart { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int Quantity { get; set; } 
+    }
+}
