e499bb9362b754f7d79006a74eb45ac2ca7d2c15
NikiRich <bogatyveron3@gmail.com>
2024-03-22 11:29:35 +0200

    v. 0.2.4
    - Updated the Order Model

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,11 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WeBoozin.Models
 {
     public class Order
     {
         [Key]
         public int OrderId { get; set; }
+
+        [ForeignKey("User")]
         public int UserId { get; set; }
+        public virtual User User { get; set; }
+
         public string Status { get; set; }
+
+        public virtual ICollection<OrderDetails> OrderDetails { get; set; }
     }
 }
