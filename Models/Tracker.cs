4ab4594de47822db14cb7293623d6de6c3c4f12f
NikiRich <bogatyveron3@gmail.com>
2024-03-22 11:30:30 +0200

    v. 0.2.7
    -Updated the Tracker Model

--- Models/Tracker.cs
+++ Models/Tracker.cs
@@ -1,13 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WeBoozin.Models
 {
     public class Tracker
     {
         [Key]
         public int TrackerId { get; set; }
+
+        [ForeignKey("User")]
         public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        [ForeignKey("Product")]
         public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
         public int Quantity { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
+
     }
 }
