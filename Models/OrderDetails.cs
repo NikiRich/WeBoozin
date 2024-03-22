29f9f12bd54b23087a1760d1ea40b5006bbc2c23
NikiRich <bogatyveron3@gmail.com>
2024-03-22 11:29:54 +0200

    v. 0.2.5
    -Updated the OrderDetails Model

--- Models/OrderDetails.cs
+++ Models/OrderDetails.cs
@@ -1,12 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WeBoozin.Models
 {
     public class OrderDetails
     {
         [Key]
         public int OrderDetailsId { get; set; }
+
+        [ForeignKey("Order")]
         public int OrderId { get; set; }
+        public virtual Order Orders { get; set; }
+
+        [ForeignKey("Product")]
         public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
         public int Quantity { get; set; }
     }
 }
