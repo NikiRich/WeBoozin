befa9fdfa3cf6030533e25a19b2d8066da165b08
NikiRich <bogatyveron3@gmail.com>
2024-03-22 11:28:30 +0200

    v. 0.2.1
    -Updated the Cart Model

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WeBoozin.Models
 {
     public class Cart
     {
         [Key]
-        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+
+        [ForeignKey("User")]
         public int UserId { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual ICollection<CartItem> CartItems { get; set; }
     }
 }
