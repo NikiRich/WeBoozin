530bf3fc1d9ba7ecebcc729c834d210120a26595
NikiRich <bogatyveron3@gmail.com>
2024-03-22 11:30:10 +0200

    v. 0.2.6
    -Updated the Product Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WeBoozin.Models
 {
     public class Product
@@ -10,6 +11,13 @@ namespace WeBoozin.Models
         public string Image { get; set; }
         public int QuantityInMl { get; set; }
         public int QuantityInStock { get; set; }
+
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<OrderDetails> OrderDetails { get; set; }
+        public virtual ICollection<CartItem> CartItems { get; set; }
+        public virtual ICollection<Tracker> Tracker { get; set; }
     }
 }
