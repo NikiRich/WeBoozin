d7000b6b5e0ea54b27cff8d5f295d87e7e0ac779
NikiRich <bogatyveron3@gmail.com>
2024-03-30 18:38:53 +0200

    v. 0.4.6
    -Added the Tequila View and Controller
    -Modified the Vodka Page and Controller

--- Pages/AlcInventory/Vodka.cshtml.cs
+++ Pages/AlcInventory/Vodka.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WeBoozin.Data;
+using WeBoozin.Models;
 
 namespace WeBoozin.Pages.Alcohol_Inventory
 {
     public class VodkaModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public VodkaModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public IList<Product> Products { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Products.Where(p => p.CategoryId == 1).Select(p => new Product
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Description = p.Description,
+                QuantityInMl = p.QuantityInMl,
+                QuantityInStock = p.QuantityInStock,
+                Image = p.Image,
+                AlcoholContent = Math.Round(p.AlcoholContent, 2),
+            }).ToListAsync();
         }
     }
 }
