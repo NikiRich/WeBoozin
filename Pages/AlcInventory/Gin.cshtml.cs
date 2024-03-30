c2c95a312499e96e95ecacd817e015b5395e69aa
NikiRich <bogatyveron3@gmail.com>
2024-03-31 00:35:16 +0200

    v. 0.5.7
    -Added the Gin View and Controller

--- Pages/AlcInventory/Gin.cshtml.cs
+++ Pages/AlcInventory/Gin.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeBoozin.Models;
+using WeBoozin.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WeBoozin.Pages.AlcInventory
 {
     public class GinModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public GinModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public IList<Product> Products { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Products.Where(p => p.CategoryId == 5).Select(p => new Product
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Description = p.Description,
+                QuantityInMl = p.QuantityInMl,
+                QuantityInStock = p.QuantityInStock,
+                Image = p.Image,
+                AlcoholContent = Math.Round(p.AlcoholContent, 2),
+            }).ToListAsync();
         }
     }
 }
