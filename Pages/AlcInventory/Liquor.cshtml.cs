ad3d88108747cec2a6f7991d7ba8e1b33397fd66
NikiRich <bogatyveron3@gmail.com>
2024-03-31 00:34:12 +0200

    v. 0.5.5
    -Added the Liquor View and Controller

--- Pages/AlcInventory/Liquor.cshtml.cs
+++ Pages/AlcInventory/Liquor.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeBoozin.Data;
+using WeBoozin.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WeBoozin.Pages.AlcInventory
 {
     public class LiquorModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        public LiquorModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public IList<Product> Products { get; private set; }
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Products.Where(p => p.CategoryId == 7).Select(p => new Product
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Description = p.Description,
+                QuantityInMl = p.QuantityInMl,
+                QuantityInStock = p.QuantityInStock,
+                Image = p.Image,
+                AlcoholContent = Math.Round(p.AlcoholContent, 2),
+            }).ToListAsync();
         }
     }
 }
