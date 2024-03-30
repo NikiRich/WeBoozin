ccd2adc96327b426bf0989a5f118ca8dc3e27b24
NikiRich <bogatyveron3@gmail.com>
2024-03-31 00:35:40 +0200

    v. 0.5.8
    -Added the Champagne View and Controller

--- Pages/AlcInventory/Champagne.cshtml.cs
+++ Pages/AlcInventory/Champagne.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WeBoozin.Data;
+using WeBoozin.Models;
 
 namespace WeBoozin.Pages.AlcInventory
 {
     public class ChampagneModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public ChampagneModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public IList<Product> Products { get; private set; }
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Products.Where(p => p.CategoryId == 9).Select(p => new Product
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Description = p.Description,
+                QuantityInMl = p.QuantityInMl,
+                QuantityInStock = p.QuantityInStock,
+                Image = p.Image,
+                AlcoholContent = Math.Round(p.AlcoholContent, 2),
+            }).ToListAsync();
         }
     }
 }
