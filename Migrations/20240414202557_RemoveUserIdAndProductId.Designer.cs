f16090be8cbae1f2629796db9165742549b090f7
NikiRich <bogatyveron3@gmail.com>
2024-04-18 19:57:09 +0300

    v. 0.9.6
    -Added new Migrations

--- Migrations/20240414202557_RemoveUserIdAndProductId.Designer.cs
+++ Migrations/20240414202557_RemoveUserIdAndProductId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeBoozin.Data;
 
@@ -11,9 +12,11 @@ using WeBoozin.Data;
 namespace WeBoozin.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240414202557_RemoveUserIdAndProductId")]
+    partial class RemoveUserIdAndProductId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240414202557_RemoveUserIdAndProductId.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeBoozin.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserIdAndProductId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tracker_Products_ProductId",
+                table: "Tracker");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tracker_Users_UserId",
+                table: "Tracker");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tracker_ProductId",
+                table: "Tracker");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tracker_UserId",
+                table: "Tracker");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Tracker");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tracker");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Tracker",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Tracker",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracker_ProductId",
+                table: "Tracker",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracker_UserId",
+                table: "Tracker",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tracker_Products_ProductId",
+                table: "Tracker",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tracker_Users_UserId",
+                table: "Tracker",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+    }
+}
