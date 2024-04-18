1d033c25ee39aaf4c9534ad93742f2f84eec19b5
NikiRich <bogatyveron3@gmail.com>
2024-04-18 19:56:44 +0300

    v. 0.9.5
    - Modified css and admin.js

---
+++ wwwroot/js/Admin.js
@@ -0,0 +1,15 @@
+﻿let img1 = document.getElementById("AlcMng")
+let img2 = document.getElementById("OrderMng")
+let img3 = document.getElementById("UserMng")
+
+img1.addEventListener("click", function () {
+    window.location.href = "/UserLogic/Admin/ProductManagement";
+})
+
+img2.addEventListener("click", function () {
+    window.location.href = "/UserLogic/Admin/OrderManagement";
+})
+
+img3.addEventListener("click", function () {
+    window.location.href = "/UserLogic/Admin/UserManagement";
+})
